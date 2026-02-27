5f61bb67d1d0d09dfc708eb1ec13c6ddfa4509ce
aobatact <aobatact144@gmail.com>
2026-02-27 17:53:35 +0900

    feat: add more specialized

--- src/macros.rs
+++ src/macros.rs
@@ -1,5 +1,5 @@
 #[macro_export]
-macro_rules! specialized_ser_via_display_inner {
+macro_rules! specialized_ser_via_display_base {
     ($ty: ty, $trait: ident) => {
         impl<S: Serializer> $trait<S> for $ty {
             fn specialized_serialize(&self, serializer: &mut S) -> Result<(), S::Error> {
@@ -10,8 +10,52 @@ macro_rules! specialized_ser_via_display_inner {
 }
 
 #[macro_export]
-macro_rules! specialized_ser_via_display {
+macro_rules! specialized_ser_via_display_inner {
     ($ty: ty) => {
-        $crate::specialized_ser_via_display_inner!($ty, $crate::SpecializedSerialize);
+        $crate::specialized_ser_via_display_base!($ty, SpecializedSerInner);
     };
 }
+
+#[macro_export]
+macro_rules! specialized_ser_via_display {
+    ($ty: ty) => {
+        $crate::specialized_ser_via_display_base!($ty, SpecializedSerialize);
+    };
+}
+
+#[macro_export]
+macro_rules! specialized_ser_via_deref_base {
+    ($ty: ty, $trait: ident) => {
+        impl<S: Serializer + 'static> $trait<S> for $ty where Self: Deref, <Self as Deref>::Target: Ser<S> {
+            fn specialized_serialize(&self, serializer: &mut S) -> Result<(), S::Error> {
+                self.deref().serialize(serializer)
+            }
+        }
+    };
+
+    ($ty: ty, $trait: ident, $($generics: tt)+) => {
+        impl<S: Serializer + 'static, $($generics)+> $trait<S> for $ty where Self: Deref, <Self as Deref>::Target: Ser<S> {
+            fn specialized_serialize(&self, serializer: &mut S) -> Result<(), S::Error> {
+                self.deref().serialize(serializer)
+            }
+        }
+    };
+}
+
+#[macro_export]
+macro_rules! specialized_ser_via_deref_inner {
+    ($ty: ty) => {
+        specialized_ser_via_deref_base!($ty, SpecializedSerInner);
+    };
+
+    ($ty: ty, $($generics: tt)+) => {
+        specialized_ser_via_deref_base!($ty, SpecializedSerInner, $($generics)+);
+    };
+}
+
+// #[macro_export]
+// macro_rules! specialized_ser_via_deref {
+//     ($ty: ty) => {
+//         $crate::specialized_ser_via_deref_inner!($ty, $crate::SpecializedSerialize);
+//     };
+// }
--- src/specialized_impls.rs
+++ src/specialized_impls.rs
@@ -1,4 +1,33 @@
 use super::*;
+use std::{ffi::OsStr, ops::Deref, path::Path};
+
+impl<T: 'static, S: Serializer + 'static> Ser<S> for [T] {
+    fn serialize(&self, serializer: &mut S) -> Result<(), S::Error> {
+        let mut seq = serializer.serialize_seq()?;
+        for elem in self {
+            seq.serialize_element(elem)?;
+        }
+        seq.end()
+    }
+}
+
+impl<S: Serializer> Ser<S> for str {
+    fn serialize(&self, serializer: &mut S) -> Result<(), S::Error> {
+        serializer.serialize_str(self)
+    }
+}
+
+impl<S: Serializer> Ser<S> for OsStr {
+    fn serialize(&self, serializer: &mut S) -> Result<(), S::Error> {
+        serializer.serialize_str(self.to_string_lossy().as_ref())
+    }
+}
+
+impl<S: Serializer> Ser<S> for Path {
+    fn serialize(&self, serializer: &mut S) -> Result<(), S::Error> {
+        serializer.serialize_str(self.as_os_str().to_string_lossy().as_ref())
+    }
+}
 
 impl<T: Ser<S>, S: Serializer> SpecializedSerInner<S> for Option<T> {
     fn specialized_serialize(&self, serializer: &mut S) -> Result<(), S::Error> {
@@ -14,18 +43,25 @@ impl<S: Serializer> SpecializedSerInner<S> for &str {
         serializer.serialize_str(self)
     }
 }
-impl<S: Serializer> SpecializedSerInner<S> for String {
-    fn specialized_serialize(&self, serializer: &mut S) -> Result<(), S::Error> {
-        serializer.serialize_str(self)
-    }
-}
 
-impl<T: 'static, S: Serializer + 'static> SpecializedSerInner<S> for Vec<T> {
-    fn specialized_serialize(&self, serializer: &mut S) -> Result<(), S::Error> {
-        self.as_slice().serialize(serializer)
-    }
-}
+specialized_ser_via_display_inner!(std::net::IpAddr);
+specialized_ser_via_display_inner!(std::net::Ipv4Addr);
+specialized_ser_via_display_inner!(std::net::Ipv6Addr);
 
-specialized_ser_via_display_inner!(std::net::IpAddr, SpecializedSerInner);
-specialized_ser_via_display_inner!(std::net::Ipv4Addr, SpecializedSerInner);
-specialized_ser_via_display_inner!(std::net::Ipv6Addr, SpecializedSerInner);
+specialized_ser_via_deref_inner!(String);
+specialized_ser_via_deref_inner!(std::path::PathBuf);
+specialized_ser_via_deref_inner!(std::ffi::OsString);
+specialized_ser_via_deref_inner!(std::borrow::Cow<'_, T>, T: ToOwned + ?Sized);
+specialized_ser_via_deref_inner!(std::vec::Vec<T>, T);
+specialized_ser_via_deref_inner!(std::boxed::Box<T>, T: ?Sized);
+specialized_ser_via_deref_inner!(std::rc::Rc<T>, T: ?Sized);
+specialized_ser_via_deref_inner!(std::sync::Arc<T>, T: ?Sized);
+specialized_ser_via_deref_inner!(std::cell::RefCell<T>, T: ?Sized);
+specialized_ser_via_deref_inner!(std::cell::RefMut<'_, T>, T: ?Sized);
+specialized_ser_via_deref_inner!(std::cell::Ref<'_, T>, T: ?Sized);
+specialized_ser_via_deref_inner!(std::sync::Mutex<T>, T: ?Sized);
+specialized_ser_via_deref_inner!(std::sync::RwLock<T>, T: ?Sized);
+specialized_ser_via_deref_inner!(std::pin::Pin<T>, T);
+specialized_ser_via_deref_inner!(std::mem::ManuallyDrop<T>, T: ?Sized);
+
+
